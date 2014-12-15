783abaf12e2b9c82eaec3acd92396d862d50a26e
Kero Yu <cyudev@gmail.com>
2014-12-15 16:21:57 +0800

    Commit 201412151621

--- _php/widgets/mediablocks.php
+++ _php/widgets/mediablocks.php
@@ -1,26 +1,26 @@
 <ul class="media-blocks">
 	<li><a href="article.php"><div>
-			<img src="//placehold.it/300x165" alt="">
+			<img src="upload/300x165.gif" alt="">
 			<h3>BBC《主題樂園》主要園區包含「福爾摩斯」和「Doctor Who」？BBC《主題樂園》主要園區包含...</h3></div>
 		</a></li><li><a href="#">
-		<div><img src="//placehold.it/300x165" alt="">
+		<div><img src="upload/300x165.gif" alt="">
 			<h3>《2014臉書電影 & 音樂排行榜》電影第一名完全不意外啊</h3></div>
 		</a></li><li><a href="#"><div>
-			<img src="//placehold.it/300x165" alt="">
+			<img src="upload/300x165.gif" alt="">
 			<h3>《惡劣天氣的散步柴犬》再糟糕的天氣也阻止不了柴柴們</h3></div>
 		</a></li></li><li><a href="#"><div>
-			<img src="//placehold.it/300x165" alt="">
+			<img src="upload/300x165.gif" alt="">
 			<h3>《2014臉書電影 & 音樂排行榜》電影第一名完全不意外啊</h3></div>
 		</a></li><li class="adbox"><a href="#">
 			<div><img src="//placehold.it/300x240" alt=""></div>
 		</a></li><li><a href="#"><div>
-			<img src="//placehold.it/300x165" alt="">
+			<img src="upload/300x165.gif" alt="">
 			<h3>《惡劣天氣的散步柴犬》再糟糕的天氣也阻止不了柴柴們再糟糕的天氣也阻止不了柴柴們《惡劣天氣的散步柴</h3></div>
 		</a></li></li><li><a href="#"><div>
-			<img src="//placehold.it/300x165" alt="">
+			<img src="upload/300x165.gif" alt="">
 			<h3>《惡劣天氣的散步柴犬》再糟糕的天氣也阻止不了柴柴們</h3></div>
 		</a></li><li><a href="#"><div>
-			<img src="//placehold.it/300x165" alt="">
+			<img src="upload/300x165.gif" alt="">
 			<h3>《2014臉書電影 & 音樂排行榜》電影第一名完全不意外啊</h3></div>
 		</a></li>
 </ul>
