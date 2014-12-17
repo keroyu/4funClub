a206724cc3d689e4fc2eae6f26d18f317fab79bd
Kero Yu <cyudev@gmail.com>
2014-12-17 16:03:49 +0800

    Add register page

---
+++ member/register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<head>
+<title>4 FUN CLUB 會員註冊</title>
+<?php require_once "_php/head.php" ?>
+</head>
+<body>
+<div class="page">
+	<?php require_once "_php/header.php" ?>
+	<div class="adbox adbox-fw hidden-md">
+		<div class="wrapper"><img src="upload/720x90.jpg" alt=""></div>
+	</div>
+	<div class="wrapper">
+		
+	</div>
+<!-- wrapper indexMain end -->
+<?php require_once "_php/footer.php" ?>
+</div>
+</body>
+</html>
