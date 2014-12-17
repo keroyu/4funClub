bd9965a7675906fe110cfac9b108dfa95c382874
Kero Yu <cyudev@gmail.com>
2014-12-17 15:41:41 +0800

    Add list page

---
+++ list.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<head>
+<title>4 FUN CLUB 首頁</title>
+<?php require_once "_php/head.php" ?>
+</head>
+<body>
+<div class="page">
+<?php require_once "_php/header.php" ?>
+<div class="adbox adbox-fw hidden-md">
+	<div class="wrapper"><img src="upload/720x90.jpg" alt=""></div>
+</div>
+<div class="wrapper">
+	<div class="row">
+		<div class="row-col col-640">
+			<?php include "_php/breadcrumb.php" ?>
+			<ul class="media-blocks bordered">
+				<li><a href="article.php"><div>
+						<img src="upload/300x165.jpg" alt="">
+						<h3>BBC《主題樂園》主要園區包含「福爾摩斯」和「Doctor Who」？BBC《主題樂園》主要園區包含...</h3></div>
+					</a></li><li><a href="#">
+					<div><img src="upload/300x165.jpg" alt="">
+						<h3>《2014臉書電影 & 音樂排行榜》電影第一名完全不意外啊</h3></div>
+					</a></li><li><a href="#"><div>
+						<img src="upload/300x165.jpg" alt="">
+						<h3>《惡劣天氣的散步柴犬》再糟糕的天氣也阻止不了柴柴們</h3></div>
+					</a></li></li><li><a href="#"><div>
+						<img src="upload/300x165.jpg" alt="">
+						<h3>《2014臉書電影 & 音樂排行榜》電影第一名完全不意外啊</h3></div>
+					</a></li><li class="adbox"><a href="#">
+						<div><img src="upload/300x240.jpg" alt=""></div>
+					</a></li><li><a href="#"><div>
+						<img src="upload/300x165.jpg" alt="">
+						<h3>《惡劣天氣的散步柴犬》再糟糕的天氣也阻止不了柴柴們再糟糕的天氣也阻止不了柴柴們《惡劣天氣的散步柴</h3></div>
+					</a></li></li><li><a href="#"><div>
+						<img src="upload/300x165.jpg" alt="">
+						<h3>《惡劣天氣的散步柴犬》再糟糕的天氣也阻止不了柴柴們</h3></div>
+					</a></li><li><a href="#"><div>
+						<img src="upload/300x165.jpg" alt="">
+						<h3>《2014臉書電影 & 音樂排行榜》電影第一名完全不意外啊</h3></div>
+					</a></li>
+			</ul>
+		</div>
+		<!-- col-640 end -->
+		<div class="row-col col-300 hidden-sm" id="rightCol">
+			<div class="adbox adbox-300"><img src="upload/300x300.jpg" alt=""></div>
+			<?php include "_php/widgets/tabsbox.php"; ?>
+			<div class="adbox adbox-300"><img src="upload/300x600.jpg" alt=""></div>
+		</div>
+		<!-- col-300 end -->
+	</div>
+	<!-- row end -->
+</div>
+<!-- wrapper indexMain end -->
+<?php require_once "_php/footer.php" ?>
+</div>
+</body>
+</html>
