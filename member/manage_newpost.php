539180b31ebce03e0a1786728ff2aba12aa05661
Kero Yu <cyudev@gmail.com>
2014-12-17 16:03:16 +0800

    Add width global classes

--- member/manage_newpost.php
+++ member/manage_newpost.php
@@ -24,9 +24,9 @@
 				<div class="bordered text-center">
 					<div class="newPostEditor">
 						<ul class="newPostEditor-leftcol tabs-vertical" data-trigger="click">
-							<li class="tabs-list-item active" data-toggle="quickPic"><a href="#">快速發圖文</a></li>
-							<li class="tabs-list-item" data-toggle="quickVid"><a href="#">快速發影片</a></li>
-							<li class="tabs-list-item" data-toggle="richText"><a href="#">多功能編輯器</a></li>
+							<li class="tabs-list-item active" data-toggle="#quickPic"><a href="#">快速發圖文</a></li>
+							<li class="tabs-list-item" data-toggle="#quickVid"><a href="#">快速發影片</a></li>
+							<li class="tabs-list-item" data-toggle="#richText"><a href="#">多功能編輯器</a></li>
 						</ul>
 						<div class="tab-pane active" id="quickPic">
 							<div class="newPostEditor-rightcol" >
@@ -37,12 +37,35 @@
 										</select>
 									</div>
 									<div class="form-field">
-										
+										<input type="file" name="datafile" placeholder="直接輸入圖片網址" size="40"><br>
+										<button>上傳</button>
+										<p class="info">上傳圖片（支援 .jpg、.png、.gif格式），檔案大小請限制在 3MB 以內</p>
 									</div>
 									<div class="form-field">
-										
+										<textarea name="" id="" cols="65" rows="10">文字敘述</textarea>
 									</div>
-									
+									<div class="form-field">
+										<input type="text" class="wid-500" placeholder="參考來源（可不填）">
+									</div>
+									<div class="form-field">
+										<input type="text" class="wid-500" placeholder="幫文章加上 tag（以逗號分隔，可不填）">
+									</div>
+								</form>
+							</div>
+						</div>
+						<div class="tab-pane" id="quickVid">
+							<div class="newPostEditor-rightcol" >
+								<form action="">
+									<div class="form-field">
+										<select name="" id="">
+											<option value="">選擇分類</option>
+										</select>
+									</div>
+									<div class="form-field">
+										<input type="file" name="datafile" placeholder="直接輸入圖片網址" size="40"><br>
+										<button>上傳</button>
+										<p class="info">上傳圖片（支援 .jpg、.png、.gif格式），檔案大小請限制在 3MB 以內</p>
+									</div>
 								</form>
 							</div>
 						</div>
