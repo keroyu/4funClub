542866d3514c4c4ff0bffc89a1810c1a6653ed0c
Kero Yu <cyudev@gmail.com>
2014-12-18 18:04:51 +0800

    Complete

---
+++ member/account/authSuccess.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<head>
+<title>4 FUN CLUB 會員註冊</title>
+<?php require_once "../../_php/head.php" ?>
+</head>
+<body>
+<div class="page">
+	<?php require_once "../../_php/header.php" ?>
+	<div class="wrapper">
+		<div class="reg">
+			<h2>會員註冊</h2>
+			<div class="bordered">
+				<p>帳號認證成功！歡迎加入會員</p>
+				<a href="/index.html" class="btn btn-secondary btn-md margin-btm">回到首頁</a>
+			</div>
+		</div>
+		
+	</div>
+<!-- wrapper indexMain end -->
+<?php require_once "../../_php/footer.php" ?>
+</div>
+</body>
+</html>
